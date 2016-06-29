125153ba99860e93ac68a0333cf6e380853c3b3e
mcelearr <rorymcelearney@gmail.com>
2016-06-29 16:56:42 +0100

    sets up unoptimised function

--- search.js
+++ search.js
@@ -1,28 +1,40 @@
 var fs = require('fs');
-var cache =[];
+// var cache =[];
 
-function getWords(letters){
+// function getWords(letters){
+//   var input = letters;
+//   var letterIndex = input.length - 1;
+//
+//   fs.readFile('example.txt', function(err, data) {
+//     if (err) throw err;
+//     if (cache.length === 0) {
+//       cache = data.toString().split("\n");
+//     };
+//     var filteredList = cache.filter(function(word){
+//       return word[letterIndex] === input[letterIndex];
+//     });
+//     console.log(filteredList);
+//     cache = filteredList;
+//   });
+// }
+
+function unoptimisedGetWords(letters){
   var input = letters;
-  var letterIndex = input.length - 1;
+  var result = [];
 
   fs.readFile('example.txt', function(err, data) {
     if (err) throw err;
-    if (cache.length === 0) {
-      cache = data.toString().split("\n");
-    };
-    var filteredList = cache.filter(function(word){
-      return word[letterIndex] === input[letterIndex];
-    });
+    var wordArray = data.toString().split("\n");
+    var filteredList = wordArray.filter(wordFilter);
     console.log(filteredList);
-    cache = filteredList;
+    result = filteredList;
   });
+
+  function wordFilter(word){
+    return word.includes(input);
+  }
+
+  return result;
 }
 
-// getWords("a");
-// getWords("ab");
-// getWords("abc");
-// getWords("abcd");
-// getWords("abcde");
-// getWords("abcdef");
-// getWords("abcdefg");
-// getWords("abcdefgh");
+console.log(unoptimisedGetWords("a"))
