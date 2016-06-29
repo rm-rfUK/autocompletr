ba1fc63341cec8be21f67e1d6830b16a51f15425
Kara de la Marck <karadelamarck@gmail.com>
2016-06-29 14:35:11 +0100

    Fix: make small edits to clean up handler.js and server.js.  Also, add package.json

--- handler.js
+++ handler.js
@@ -7,14 +7,16 @@ function handler (request, response) {
   if (endpoint === "/") {
     response.writeHead(200, {"Content-Type": "text/html"});
 
-    fs.readFile('index.html', function(error, file) {
+    fs.readFile(__dirname + '/index.html', function(error, file) {
       if (error) {
         return console.log(error);
       }
-
       response.end(file);
     });
-  }
+  // } else if (endpoint === ".css") {
+      // response.writeHead(200, {"Content-Type": appropriate type });
+     // do the thing
+ }
 }
 
 
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const handler = require('./handler.js');
 const server = http.createServer(handler);
 const fs = require('fs');
-// const handler = require('./handler.js');
 
 const port = 3000;
 server.listen(port);
