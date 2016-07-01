0c487f9aa2b55f16f611408b7ff6f0d6b3ac9a41
mcelearr <rorymcelearney@gmail.com>
2016-07-01 17:31:57 +0100

    fixes create table function so clears the table before each call

--- request.js
+++ request.js
@@ -10,8 +10,12 @@ function getWords(e){
   xhr.onreadystatechange = function () {
     if(xhr.readyState === 4 && xhr.status === 200){
     var result = JSON.parse(xhr.responseText);
+    var table = document.getElementById('suggestions');
+    while (table.firstChild) {
+      table.removeChild(table.firstChild);
+    }
       for(var x = 0; x < result.length; x++){
-        var table = document.getElementById('suggestions');
+        table.insertRow(0);
         var row = table.insertRow(x + 1);
         var cell = row.insertCell(0);
         cell.innerHTML = result[x];
