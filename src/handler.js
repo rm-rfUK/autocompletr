f75d3396d043fe6d972713541423a68fa626340f
mcelearr <rorymcelearney@gmail.com>
2016-07-01 13:13:27 +0100

    sets event listener in request.js

--- src/handler.js
+++ src/handler.js
@@ -10,7 +10,7 @@ function handler(request, response) {
 // } else if (endpoint === ".css") {
       // response.writeHead(200, {"Content-Type": appropriate type });
      // do the thing
-  } else if (endpoint.includes('/create-response')) {
+  } else if (endpoint.includes('?')) {
     createResponse(request, response);
   } else {
     response.writeHead(404);
@@ -29,15 +29,19 @@ function renderIndexHtml(request, response) {
 }
 
 function createResponse(request, response) {
-  let allTheData = '';
+  const url = request.url;
+  console.log(url);
+  let allTheData = request.url.split("=")[1];
+  console.log(allTheData);
 
   request.on('data', function (chunkOfData) {
-      allTheData += chunkOfData;
+    allTheData += chunkOfData;
   });
+
   request.on('end', function () {
-    console.log(allTheData);
+    // console.log(allTheData);
     const convertedData = querystring.parse(allTheData);
-    console.log(convertedData);
+    // console.log(convertedData);
     response.writeHead(205, {'Location': '__dirname + "/index.html"'});
     response.end();
   });
