36caafdf092c441b7fde8b3bb9a3d6443a590721
Kara de la Marck <karadelamarck@gmail.com>
2016-07-04 10:06:49 +0100

    Fix:  reorder code in reder.js so more legible

--- src/render.js
+++ src/render.js
@@ -3,35 +3,7 @@ var createTrieGraph = require('./algorithmTrie');
 //Tie all the functions together to actually do the autocomplete!
 
 
-//take data from words.txt and split on each new line to make array of words, to be used to create trie graph
-
-// function makeArray(string){
-//   var arrayWords = string.split('\n');
-//   return arrayWords;
-// };
-//
-//
-// //convert array element into string
-// function stringifyArray(input){
-//   return input.join(',');
-// }
-//
-// function randomWordsFromArray(autocompletedWordsArray) {
-//   var numberWordsWantReturned = 3;
-//   var arrayLength = autocompletedWordsArray.length;
-//   var randomWordsArray = [];
-//
-//   if (arrayLength > numberWordsWantReturned) {
-//     for(var i = 1; i <= numberWordsWantReturned; i++) {
-//       var randomIndex = Math.floor(Math.random() * arrayLength);
-//       var chosenWord = autocompletedWordsArray[randomIndex];
-//       randomWordsArray.push(chosenWord);
-//     }
-//     return randomWordsArray;
-//   } else {
-//     return autocompletedWordsArray;
-//   }
-// }
+//take data from words.txt and split on each new line to make array of words, to be used to create trie graph and then search for randomly selected autocompleted words array.
 
 //render all the data connecting all the functions together
 function renderData(input, searchWord){ //input = fileAsString(data)
